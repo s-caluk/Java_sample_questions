6c2553fa6517d972325d9ed0e1018676e3dc229c
s-caluk <seher.caluk@gmail.com>
2023-01-08 00:12:08 +0100

    S07

--- src/Sorular_01_10/S07.java
+++ src/Sorular_01_10/S07.java
@@ -1,4 +1,30 @@
 package Sorular_01_10;
 
+import java.util.Scanner;
+
 public class S07 {
+    public static void main(String[] args) {
+        /*
+        Soru 7-)
+        Kullanicidan Double türünde bir sayi alin. Alinan bu sayiyi tam sayiya cevirerek konsolda yazdirin.
+        (Istege bagli) Float degerinde bir sayi alin bu sayiyi da short degisken türünde bir sayiya döndürün
+        ipuclari:
+        Data Casting konusuna bakiniz. Auto Widening ve Explicit Narrowing konularini hatirlayalim.
+         */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen bir sayi giriniz: ");
+        double sayi = scan.nextDouble();
+
+        int sayiCast = (int)sayi;
+        System.out.println("Girdiginiz sayının tam sayı hali : "+sayiCast);
+
+
+        System.out.println("=====================2.cözüm======================");
+
+        System.out.println("Lütfen float türünde bir sayı girin: ");
+        float deger=scan.nextFloat();
+        short sayiS= (short)deger;
+        System.out.println("Girdiginiz sayının en son hali : " + sayiS);
+    }
 }
