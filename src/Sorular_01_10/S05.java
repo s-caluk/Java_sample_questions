df0328d7e1260f01a5411434cf29316bdf8c34b3
s-caluk <seher.caluk@gmail.com>
2023-01-07 19:41:58 +0100

    S05

--- src/Sorular_01_10/S05.java
+++ src/Sorular_01_10/S05.java
@@ -1,4 +1,22 @@
 package Sorular_01_10;
 
+import java.util.Scanner;
+
 public class S05 {
+    public static void main(String[] args) {
+        /*
+        Soru 5-)   SCANNER
+        Kullanıcıdan karenin kenar uzunluğunu alın. Aldiginiz uzunluklarla karenin alanını ve çevresini hesaplayan kodu yazın.
+        Ipuclari:
+        Google yaparak karenin alaninin ve cevresinin nasil bulundugunu ögrenin :-)
+         */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen karenin kenar uzunlugunu giriniz");
+        int kenar = scan.nextInt();
+
+        System.out.println("Alan: "+kenar*3);
+        System.out.println("Cevre: "+kenar*4);
+
+    }
 }
